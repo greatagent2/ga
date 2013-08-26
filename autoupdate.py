b14343fda38de096872bdeebf0c46d27e8d2bd23
wwqgtxx <wwqgtxx@gmail.com>
2013-08-26 10:41:34 +0800

    bat

--- autoupdate.py
+++ autoupdate.py
@@ -165,7 +165,7 @@ class Updater(object):
 		file = self.getfile(filename)
 		path = self.dir+filename
 		old_file_sha1 = sha1.getconfig('FILE_SHA1','$path$'+filename)
-		new_file_sha1 = FileUtil.sumfile(file)
+		new_file_sha1 = FileUtil.get_file_sha1(file)
 		if not old_file_sha1 == new_file_sha1:
 			output = open(newpath)
 			output.write(file,"wb")
