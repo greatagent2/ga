fad33a984565e957f6ff990190615527549dbdbb
wwqgtxx <wwqgtxx@gmail.com>
2013-08-27 15:18:55 +0800

    bat

--- common.py
+++ common.py
@@ -149,7 +149,8 @@ class Common(object):
 	def info(self):
 		info = ''
 		info += '------------------------------------------------------\n'
-		info += 'GreatAgent Version	: %s (python/%s %spyopenssl/%s rsa/%s PyASN1/%s base92/%s)\n' % (__version__, sys.version[:5], gevent and 'gevent/%s ' % gevent.__version__ or '', getattr(OpenSSL, '__version__', 'Disabled'),rsa.__version__,pyasn1.__version__,base92.__version__)
+		info += 'GreatAgent Version	: %s \n'% (__version__)
+		info += 'RunTime            :python/%s %spyopenssl/%s rsa/%s PyASN1/%s base92/%s\n' % (sys.version[:5], gevent and 'gevent/%s ' % gevent.__version__ or '', getattr(OpenSSL, '__version__', 'Disabled'),rsa.__version__,pyasn1.__version__,base92.__version__)
 		info += 'Update Server      : %s\n' % '|'.join(self.AUTOUPDATE_SERVER)
 		info += '------------------------------------------------------\n'
 		return info
