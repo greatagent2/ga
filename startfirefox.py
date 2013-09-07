8b2b3756d8f08ec8ed0a0f140b45e31d7e6da5ed
wwqgtxx <wwqgtxx@gmail.com>
2013-09-07 13:52:51 +0800

    bat

--- startfirefox.py
+++ startfirefox.py
@@ -12,7 +12,7 @@ def main():
 	dir = FileUtil.cur_file_dir()
 	os.chdir(dir)
 	if FileUtil.has_file('FirefoxPortable/FirefoxPortable.exe'):
-		os.system('start ./FirefoxPortable/FirefoxPortable.exe  -no-remote "https://greatagent-ifanqiang.googlecode.com/git-history/web/ifanqiang2.htm"')
+		os.system('start ./FirefoxPortable/FirefoxPortable.exe  -no-remote "https://greatagent-ifanqiang.googlecode.com/git-history/web/ifanqiang3.htm"')
 	else:
 		print "Don't Have FirefoxPortable"
 		FileUtil.delete_dir("FirefoxPortable")
